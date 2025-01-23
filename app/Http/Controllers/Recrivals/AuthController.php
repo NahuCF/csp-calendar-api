375a19ac91e3d9e84bd60f4b06e3357f4e1f71c6
Nahuel <nahuefer173@gmail.com>
2025-01-22 21:11:40 -0300

    Create client on tenant when guest signs up

--- app/Http/Controllers/Recrivals/AuthController.php
+++ app/Http/Controllers/Recrivals/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Recrivals;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Models\Client;
 use App\Models\Tenant;
 use App\Models\User;
 use App\Services\IdentifierService;
@@ -19,11 +20,19 @@ class AuthController extends Controller
     {
         $input = $request->validate([
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'cellphone' => ['required', 'string', 'max:255'],
+            'prefix' => ['required', 'string', 'max:255'],
             'password' => ['required',  'string'],
+            'identifier' => ['required',  'string'],
         ]);
 
         $email = data_get($input, 'email');
         $password = data_get($input, 'password');
+        $cellphone = data_get($input, 'cellphone');
+        $prefix = data_get($input, 'prefix');
+        $firstName = data_get($input, 'first_name');
+        $identifier = data_get($input, 'identifier');
 
         if (User::query()->where('email', strtolower($email))->exists()) {
             throw ValidationException::withMessages([
@@ -38,11 +47,24 @@ class AuthController extends Controller
             'identifier' => IdentifierService::generate(),
         ]);
 
+        $tenantOwner = Tenant::query()
+            ->where('identifier', $identifier)
+            ->first();
+
         $user = User::create([
             'email' => strtolower($email),
             'tenant_id' => $tenantId,
             'password' => Hash::make($password),
         ]);
+
+        Client::query()
+            ->create([
+                'name' => $firstName,
+                'cellphone' => $cellphone,
+                'prefix' => $prefix,
+                'tenant_id' => $tenantOwner->id,
+                'user_id' => $user->id,
+            ]);
 
         $user->assignRole('Guest');
         $role = Role::where('name', 'Guest')->first();
