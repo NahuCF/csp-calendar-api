388fca0edf9158546ff494d571f1f9cd76b74004
Nahuel <nahuefer173@gmail.com>
2025-01-22 21:10:07 -0300

    Add billable to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, HasRoles, Notifiable, SoftDeletes;
+    use Billable, HasApiTokens, HasFactory, HasRoles, Notifiable, SoftDeletes;
 
     protected $guarded = [];
 
